6ed8992fb780cfc686ffe7f1a94354d9d8173fe9
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-06-26 15:15:43 -0600

    calculator can add 2 numbers

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = new double[2];
+            string operationType;
+            string result;
+
+            Console.WriteLine("What operation would you like to preform?");
+            Console.WriteLine("add, subtract, multiply, divide");
+            Console.Write("> "); operationType = Console.ReadLine();
+
+            Console.WriteLine("Enter first number");
+            Console.Write("> "); inputNumbers[0] = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            Console.Write("> "); inputNumbers[1] = Convert.ToDouble(Console.ReadLine());
+
+            if (operationType.ToLower() == "add" || operationType.ToLower() == "addition")
+            {
+                result = Convert.ToString(Add(inputNumbers));
+            } else {
+                result = "Error";
+            }
+
+
+
+            Console.WriteLine("The result is: " + result);
+
+        }
+
+        private static double Add(double[] args)
+        {
+            double[] addends = {args[0], args[1]};
+            double sum;
+
+            sum = addends[0] + addends[1];
+
+            return sum;
+        }
+    }
+}
