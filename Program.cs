db2b4e6cbd946d992c284aac9b41b20e5bfe9d4c
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-06-26 16:00:11 -0600

    added all four dunctions and loop

--- Program.cs
+++ Program.cs
@@ -9,26 +9,44 @@ namespace Calculator
             string operationType;
             string result;
 
-            Console.WriteLine("What operation would you like to preform?");
-            Console.WriteLine("add, subtract, multiply, divide");
-            Console.Write("> "); operationType = Console.ReadLine();
+            while (true) {
 
-            Console.WriteLine("Enter first number");
-            Console.Write("> "); inputNumbers[0] = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What operation would you like to preform?");
+                Console.WriteLine("add, subtract, multiply, divide, quit");
+                Console.Write("> "); operationType = Console.ReadLine();
 
-            Console.WriteLine("Enter second number");
-            Console.Write("> "); inputNumbers[1] = Convert.ToDouble(Console.ReadLine());
+                if (operationType.ToLower() == "quit" || operationType.ToLower() == "exit" || operationType.ToLower() == "end")
+                {
+                    break;
+                }
 
-            if (operationType.ToLower() == "add" || operationType.ToLower() == "addition")
-            {
-                result = Convert.ToString(Add(inputNumbers));
-            } else {
-                result = "Error";
+                Console.WriteLine("Enter first number");
+                Console.Write("> "); inputNumbers[0] = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                Console.Write("> "); inputNumbers[1] = Convert.ToDouble(Console.ReadLine());
+
+                if (operationType.ToLower() == "add" || operationType.ToLower() == "addition")
+                {
+                    result = Convert.ToString(Add(inputNumbers));
+                } 
+                else if (operationType.ToLower() == "subtract" || operationType.ToLower() == "subtraction")
+                {
+                    result = Convert.ToString(Subtract(inputNumbers));
+                }
+                else if (operationType.ToLower() == "multiply" || operationType.ToLower() == "multiplication")
+                {
+                    result = Convert.ToString(Multiply(inputNumbers));
+                }
+                else if (operationType.ToLower() == "divide" || operationType.ToLower() == "division")
+                {
+                    result = Convert.ToString(Divide(inputNumbers));
+                } else {
+                    result = "Error. Try again.";
+                }
+
+                Console.WriteLine("The result is: " + result);
             }
-
-
-
-            Console.WriteLine("The result is: " + result);
 
         }
 
@@ -40,6 +58,39 @@ namespace Calculator
             sum = addends[0] + addends[1];
 
             return sum;
+        }
+
+        private static double Subtract(double[] args)
+        {
+            double minuend = args[0];
+            double subtrahend = args[1];
+            double difference;
+
+            difference = minuend - subtrahend;
+            
+            return difference;
+        }
+
+        private static double Multiply(double[] args)
+        {
+            double multiplicand = args[0];
+            double multiplier = args[1];
+            double product;
+
+            product = multiplicand * multiplier;
+
+            return product;
+        }
+        
+        private static double Divide(double[] args)
+        {
+            double dividend = args[0];
+            double divisor = args[1];
+            double quotient;
+
+            quotient = dividend / divisor;
+
+            return quotient;
         }
     }
 }
