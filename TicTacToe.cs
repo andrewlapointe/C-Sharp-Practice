82744796d8af81ad1aaae41e0ac45308cfb27d85
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-07-01 16:42:10 -0600

    added win logic

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -5,8 +5,10 @@ namespace TicTacToe
         private string currentGameBoard;
         private string currentPlayer = "X";
         private bool gameIsOver = false;
+        private string[] defaultPos = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
         private string[] pos = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
         private string gameBoard; 
+        private string? play;
 
 
         public TicTacToe()
@@ -16,15 +18,26 @@ namespace TicTacToe
         public void Main()
         {   
             int move;
-            while (!gameIsOver)
+            while (true)
             {
-                updateDisplayString();
+                pos = defaultPos;
+                while (!gameIsOver)
+                {
+                    nextPlayer();
+                    updateDisplayString();
+                    displayString();
+                    Console.WriteLine(String.Format("{0}'s turn. Which space will you take?: ", currentPlayer));
+                    move = Convert.ToInt16(Console.ReadLine());
+                    pos[move] = currentPlayer;
+                    checkForWin();
+                }
                 displayString();
-                Console.WriteLine(String.Format("{0}'s turn. Which space will you take?: ", currentPlayer));
-                move = Convert.ToInt16(Console.ReadLine());
-                Console.WriteLine(move);
-                pos[move] = currentPlayer;
-                nextPlayer();
+                Console.WriteLine(String.Format("Game Over. {0} won!", currentPlayer));
+                Console.Write("Play Again? [y/n]: "); play = Console.ReadLine().ToLower();
+                if (play == "n")
+                {
+                    break;
+                }
             }
         }
 
@@ -53,6 +66,69 @@ namespace TicTacToe
                 currentPlayer = "O";
             } else {
                 currentPlayer = "X";
+            }
+        }
+        private void checkForWin()
+        {
+            int lineHead = -1;
+
+            if (pos[4] == currentPlayer)  // Check the center
+            {
+                for(int i = 0; i < 4; i++)  // Check the start of each possible line from center
+                {
+                    if (pos[i] == currentPlayer)
+                    {
+                        lineHead = i;
+                        break;
+                    }
+                }
+                switch (lineHead)  // Checks each of the 4 win cases with the center
+                {
+                    case 0:
+                        if (pos[8] == currentPlayer) {gameIsOver = true;}
+                        return;
+                    case 1:
+                        if (pos[7] == currentPlayer) {gameIsOver = true;}
+                        return;
+                    case 2:
+                        if (pos[6] == currentPlayer) {gameIsOver = true;}
+                        return;
+                    case 3:
+                        if (pos[5] == currentPlayer) {gameIsOver = true;}
+                        return;
+                    default:
+                        break;
+                    
+                }
+            }
+            else if (pos[0] == currentPlayer)
+            {
+                if (pos[2] == currentPlayer)
+                {
+                    lineHead = 2;
+                    if (pos[1] == currentPlayer) {gameIsOver = true;}
+                    return;
+                }
+                else if (pos[6] == currentPlayer)
+                {
+                    lineHead = 6;
+                    if (pos[3] == currentPlayer) {gameIsOver = true;}
+                    return;
+                } else {
+                    return;
+                }
+            }
+            else if (pos[8] == currentPlayer)
+            {
+                switch (lineHead)
+                {
+                    case 2:
+                        if (pos[5] == currentPlayer) {gameIsOver = true;}
+                        return;
+                    case 6:
+                        if (pos[3] == currentPlayer) {gameIsOver = true;}
+                        return;
+                }
             }
         }
     }
